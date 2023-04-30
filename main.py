757b3b8491906243185d241e7d7afd2519f02b7b
Gyanesh Raj <93124096+Gyanesh41@users.noreply.github.com>
2023-04-30 14:44:40 +0530

    main.py
    Added features:
    specific data location can be deleted (one at a time)
    random data from specific row and column can be deleted
    
    Previous features:
    random data from whole dataset can be deleted

--- main.py
+++ main.py
@@ -1,37 +1,57 @@
 import pandas as pd;
 import random
 
-argdict={'percentage':None,'data_location':None,'data':None,'savename':None,'missingindexset':None,'mvnum':None}
+argdict={'percentage':None,'data_location':None,'column':None,'row':None,'data':None,'savename':None,'missingindexset':None,'mvnum':None}
 
 def mvgenerator(argdict):
-    preinputs(argdict);
-    generator(argdict);
-    data=argdict['data'];
-    savename=input("Please provide path and file name with extensionto save datasheet : ")
-    if savename=="":data.to_csv("FinalDatasheet.csv");
+    preinputs(argdict)
+    generator(argdict)
+    data=argdict['data']
+    savename=input("Please provide path and file name with extension to save datasheet : ")
+    if savename=="":data.to_csv("FinalDatasheet.csv")
     else:data.to_csv(savename)
-    print(data.isnull().sum().sum())
-    
+    print("Missing Values : ",data.isnull().sum().sum())
+        
 def preinputs(argdict):
-    percentage=float(input("Please provide percentage of data you want missing : "));
-    data_location=input("Enter location of data with file exensions : ");
-    argdict.update([('percentage',percentage),('data_location',data_location)]);
-   
+    percentage=float(input("Please provide percentage of data you want missing (for specific cell provide 100 % ) : "))
+    data_location=input("Enter location of data with file exensions : ")
+    data=pd.DataFrame(pd.read_csv(data_location))
+    column=False;row=False
+    ask=input('Do you want to add missing values in any specific column or row ( Y / N ) : ').casefold()
+    if ask=='y':
+        exit=0
+        while (exit==0):
+            colrow=input("Please provide if you want use column or row \n[ 'col' : column / 'row' : row / 'exit' : exit options]: ").casefold()
+            if colrow=='col':column=input("Please provide column name : ")
+            elif colrow=='row':row=int(input("Please provide row number : "))
+            elif colrow=='exit':exit=1
+            else:print("Wrong Input")
+    argdict.update([('percentage',percentage),('data_location',data_location),('data',data),('column',column),('row',row)])
+    
+
 def generator(argdict):
-    percentage=argdict['percentage'];
-    data=pd.read_csv(argdict['data_location']);
-    shape=list(data.shape);
-    mvnum=int(percentage*shape[0]*shape[1]//100);
-    column=data.columns;
-    i=1;missingset=set();
+    percentage=argdict['percentage']
+    data=argdict['data']
+    shape=list(data.shape)
+    if argdict['column'] ==False:column=argdict['column']
+    else:column=argdict['column'];shape[1]=1
+    if argdict['row']==False:row=argdict['row']
+    else:row=argdict['row'];shape[0]=1
+    columnlist=list(data.columns)
+    mvnum=int(percentage*shape[0]*shape[1]//100)
+    i=1;missingset=set()
     while (i<=mvnum):
-        row=random.randint(0,shape[0]-1);
-        columnindex=random.randint(0,shape[1]-1);
-        if (row,columnindex) in missingset:i=i-1;
+        if argdict['row']!=False:row=argdict['row']
+        else:row=random.randint(1,shape[0])
+        if argdict['column']!=False:columnindex=columnlist.index(column);columnindex+=1
+        else:columnindex=random.randint(1,shape[1])
+        if (row,columnindex) in missingset:i=i-1
         else:
-            data[column[columnindex]][row]=None;
-            missingset.update({(row,columnindex)});
-        i=i+1;
+            data.loc[row-1,columnlist[columnindex-1]]=None
+            missingset.update({(row,columnindex)})
+        i=i+1
+    print("Total data where missing values is getting generated : ",shape[0]*shape[1])
     argdict.update([('missingindexset',missingset),('mvnum',mvnum),('data',data)])
+    print(missingset)
 
 mvgenerator(argdict)
